9d7063cd59c06420fe72f119304bffe0ba349901
Chase <chasevriend84@gmail.com>
2023-02-06 12:43:22 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let wDrum = document.getElementsByClassName("w drum");
+let aDrum = document.getElementsByClassName("a drum");
+let sDrum = document.getElementsByClassName("s drum");
+let dDrum = document.getElementsByClassName("d drum");
+let jDrum = document.getElementsByClassName("j drum");
+let kDrum = document.getElementsByClassName("k drum");
+let lDrum = document.getElementsByClassName("l drum");
+
+let playSound = () => new Audio("src").play();
